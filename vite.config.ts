9a2f564a2b775fe471edefc0b3260719b142f9ea
hvaishnav27 <himanshuvaishnav1818@gmail.com>
2026-05-24 21:24:28 +0530

    adding myportfolio to "/".

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/',  // Add this line
+  base: '/MyPortfolio/',  // ← Change this
   plugins: [react(), tailwindcss()],
 })
-
